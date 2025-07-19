6903140f3a49429c131017fdfee3988a461f1d2d
hoangndhe161694 <hoangndhe161694@fpt.edu.vn>
2025-07-19 23:26:32 +0700

    update tree be

--- server/services/tree.service.js
+++ server/services/tree.service.js
@@ -2,6 +2,7 @@ const Tree = require("../models/tree.model");
 const Garden = require("../models/garden.model");
 const NotFoundException = require("../middlewares/exceptions/notfound");
 const BadRequestException = require("../middlewares/exceptions/badrequest");
+const mongoose = require("mongoose");
 
 const genArrayData = (row, col) => {
   let data = [];
@@ -44,7 +45,32 @@ const generateTree = async (gardenId, row, col) => {
 };
 
 const getList = async (gardenId) => {
-  const trees = await Tree.find({ gardenId: gardenId });
+  const trees = await Tree.aggregate([
+    {
+      $match: {
+        gardenId: new mongoose.Types.ObjectId(gardenId),
+      },
+    },
+    {
+      $lookup: {
+        from: "taskquestions",
+        localField: "_id",
+        foreignField: "treeId",
+        as: "questions",
+      },
+    },
+    {
+      $addFields: {
+        questionCount: { $size: "$questions" },
+      },
+    },
+    {
+      $project: {
+        questions: 0,
+      },
+    },
+  ]);
+
   return trees;
 };
 
